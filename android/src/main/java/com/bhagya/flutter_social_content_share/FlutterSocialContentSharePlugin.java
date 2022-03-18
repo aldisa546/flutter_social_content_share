4485d357851bd553b4e8f80e6b22f5d9f0df6c03
Daffa Shidiq <daffashi00@gmail.com>
2022-03-18 15:14:01 +0700

    use imageName for saving instagram image

--- android/src/main/java/com/bhagya/flutter_social_content_share/FlutterSocialContentSharePlugin.java
+++ android/src/main/java/com/bhagya/flutter_social_content_share/FlutterSocialContentSharePlugin.java
@@ -194,7 +194,7 @@ public class FlutterSocialContentSharePlugin implements FlutterPlugin, MethodCal
     }
     ByteArrayOutputStream bytes = new ByteArrayOutputStream();
     inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-    String path = MediaStore.Images.Media.insertImage(activity.getContentResolver(), inImage,"IMG_" + Calendar.getInstance().getTime(),null);
+    String path = MediaStore.Images.Media.insertImage(activity.getContentResolver(), inImage,"IMG_" + imageName,null);
     return Uri.parse(path);
   }
 
